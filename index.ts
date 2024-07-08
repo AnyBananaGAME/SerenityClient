37fc3d45e79a81a9044eef9060205b10562592d7
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-07-09 02:38:56 +0300

    add exports to index.ts

--- index.ts
+++ index.ts
@@ -1,18 +1,10 @@
 import "reflect-metadata"
-import { Server } from "@serenityjs/raknet";
 import Client from "./src/Client";
-import { DisconnectPacket, DisconnectReason } from "@serenityjs/protocol";
+import { DisconnectPacket } from "@serenityjs/protocol";
+import RakNetClient from "./src/client/RaknetClient";
+
 
 async function a(){
-   /*
-    const server = new Server("127.0.0.1", 19132);
-    server.on("connect", () => console.log("Connected!"));
-    server.on("disconnect", () => console.log("Disconnected!"))
-    await server.start();
-    server.socket.on("message", (msg, rinfo) => {
-        if(msg.length != 33) console.log(msg, rinfo)
-    })
-   */
     setTimeout(async () => {
         console.log("[Client] " + new Date().toLocaleDateString())
         const client = new Client({host: "127.0.0.1", port: 19132});
@@ -22,8 +14,9 @@ async function a(){
                 process.exit()
             }, 1000)
         })
-        //await client.raknet.ping();
         await client.connect();
     }, 1000)
 }
 a()
+
+export { Client, RakNetClient } 
